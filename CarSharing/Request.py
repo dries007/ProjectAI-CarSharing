fe267565b8f551cf489695dbd166f07afd88f56a
Olivier <oliviervandeneede@hotmail.com>
2019-02-18 09:59:17 +0100

    Base classes for request and zone

---
+++ CarSharing/Request.py
@@ -0,0 +1,11 @@
+
+class Request:
+    def __init__(self, id, zone, day, start, duration, vehicules, penalty1, penalty2):
+        self.id = id
+        self.zone = zone
+        self.day = day
+        self.start = start
+        self.duration = duration
+        self.vehicules = vehicules
+        self.penalty1 = penalty1
+        self.penalty2 = penalty2
---
+++ CarSharing/Zone.py
@@ -0,0 +1,5 @@
+
+class Zone:
+    def __init__(self, id, neighbours):
+        self.id = id
+        self.neighbours = neighbours
