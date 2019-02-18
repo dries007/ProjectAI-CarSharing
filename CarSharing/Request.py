3f6940808693424b8b9c0d5e056d00dfa6a4acf1
Dries007 <admin@dries007.net>
2019-02-18 10:02:02 +0100

    fixed typo & added types

--- CarSharing/Request.py
+++ CarSharing/Request.py
@@ -1,11 +1,11 @@
 
 class Request:
-    def __init__(self, id, zone, day, start, duration, vehicules, penalty1, penalty2):
+    def __init__(self, id: str, zone: str, day: int, start: int, duration: int, vehicles: [str], penalty1: int, penalty2: int):
         self.id = id
         self.zone = zone
         self.day = day
         self.start = start
         self.duration = duration
-        self.vehicules = vehicules
+        self.vehicles = vehicles
         self.penalty1 = penalty1
         self.penalty2 = penalty2
--- CarSharing/Zone.py
+++ CarSharing/Zone.py
@@ -1,5 +1,5 @@
 
 class Zone:
-    def __init__(self, id, neighbours):
+    def __init__(self, id: str, neighbours: [str]):
         self.id = id
         self.neighbours = neighbours
