446c943e5bddc8ff4a597341044d0d40df23c14c
Olivier <oliviervandeneede@hotmail.com>
2019-04-22 13:47:07 +0200

    Simulated Annealing and new scores

--- CarSharing/Problem.py
+++ CarSharing/Problem.py
@@ -67,50 +67,58 @@ class Problem:
         i = 0
         global_best = solution
         working_solution = solution.copy()
-        i_since_last_improvement = 0
-        stale_factor = 100 * len(self.requests)
+
+        # Simulated Annealing parameters
+        t_max = 1000            # Max temp
+        t_min = 10              # Min temp
+        iterations = 1000       # Iterations for equilibrium
+        alpha = 0.65            # Cooling factor
+
+        temp = t_max
+
         aborted = False
         try:
-            # self.log.debug('Started actually iterating...')
-            while True:
-                # todo: add any extra "move" functions here. They should all have the signature '() -> bool'
-                # todo: Maybe make some moves more likely than others. The 'harsher' the change,
-                #  the more likely they will have a large impact, but also the more compute power is required.
-                func = self.rng.choice((
-                    working_solution.move_to_neighbour,
-                    working_solution.neighbour_to_self,
-                    working_solution.change_car_in_zone,
-                    working_solution.unassign_request,
-                    working_solution.unassign_request,  # 2x more likely
-                    working_solution.unassign_car,
-                    working_solution.unassign_car,  # 2x more likely
-                ))
+            # Simulated Annealing
+            while temp >= t_min:                    # Iterate until stopcondition is reached
+                for x in range(iterations):         # Iterate until equilibrium is reached
+                    # todo: add any extra "move" functions here. They should all have the signature '() -> bool'
+                    # todo: Maybe make some moves more likely than others. The 'harsher' the change,
+                    #  the more likely they will have a large impact, but also the more compute power is required.
+                    func = self.rng.choice((
+                        working_solution.move_to_neighbour,
+                        working_solution.neighbour_to_self,
+                        working_solution.change_car_in_zone,
+                        working_solution.unassign_request,
+                        working_solution.unassign_request,  # 2x more likely
+                        working_solution.unassign_car,
+                        working_solution.unassign_car,  # 2x more likely
+                    ))
 
-                if func():
-                    working_solution.calculate_cost()
-                    if working_solution.cost < global_best.cost:
-                        global_best = working_solution
-                        solution = working_solution
-                        i_since_last_improvement = 0
-                    # todo: implement simulated annealing, not this random 'homebrew' thing
-                    # Accept worse thing with a chance that's depending on when the last improvement was.
-                    elif self.rng.random() < 0.001*math.exp(-0.001*i_since_last_improvement):
-                        solution = working_solution
-                    # Also have some chance to reset to the global best.
-                    # Lower at the start, but it catches up at ~380, it passes 1 after ~1842
-                    elif self.rng.random() < 0.0001*math.exp(0.005*i_since_last_improvement):
-                        solution = global_best
+                    # Generate random neighbour of solution
+                    if func():
+                        # Calculate energie difference
+                        working_solution.calculate_cost()
+                        delta_e = working_solution.cost - global_best.cost
+
+                        if delta_e <= 0:
+                            # This solution is better, save it
+                            global_best = working_solution
+                            solution = working_solution
+
+                        else:
+                            # New solution is worse, accept it anyway with a probability
+                            if math.exp(-delta_e / temp) > self.rng.random():
+                                global_best = working_solution
+                                solution = working_solution
 
                     working_solution = solution.copy()
 
-                i += 1
-                i_since_last_improvement += 1
-                if debug:
-                    stats.append(working_solution.cost)
+                    i += 1
+                    if debug:
+                        stats.append(working_solution.cost)
 
-                # Todo: Fine-tune
-                if i_since_last_improvement > stale_factor:
-                    break
+                # Apply cooling and reduce temp
+                temp = temp * alpha
 
         except (KeyboardInterrupt, TimeoutError):
             aborted = True
